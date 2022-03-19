6db609a27ebeb1a6dda750dd01906675a8f19261
ALEX321616 <zaplev@ukr.net>
2022-03-19 22:28:08 +0200

    дорабтка

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ApiService from './ApiService/ApiService';
 
 import Modal from './Modal/Modal';
@@ -9,7 +9,6 @@ import Loader from './Loader/Loader';
 import { ToastContainer, toast, Zoom } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-// *******************************************
 const App = () => {
   const [searchImg, setSearchImg] = useState('');
   const [page, setPage] = useState(1);
@@ -20,19 +19,21 @@ const App = () => {
   const [showModal, setShowModal] = useState(false);
   const [largeImageURL, setLargeImageURL] = useState('');
   const [alt, setAlt] = useState('');
+  const totalHits = useRef(0);
 
   useEffect(() => {
     if (searchImg) {
       setLoading(true);
 
       ApiService(searchImg, page)
-        .then(({ hits }) => {
-          if (hits.length === 0) return toast('No images show');
-          if (hits) {
-            setButtonShow(hits.length === 12 ? true : false);
-            setImagesData(prevImg => [...prevImg, ...hits]);
-          }
-          return hits;
+        .then(data => {
+          if (data.hits.length === 0) return toast('No images show');
+          totalHits.current = totalHits.current + data.hits.length;
+
+          setButtonShow(totalHits.current !== data.totalHits ? true : false);
+          setImagesData(prevImg => [...prevImg, ...data.hits]);
+
+          return data.hits;
         })
         .catch(error => {
           alert('error');
@@ -47,6 +48,7 @@ const App = () => {
     setImagesData([]);
     setSearchImg(nameImg);
     setButtonShow(false);
+    totalHits.current = 0;
   };
 
   const addImg = () => {
@@ -54,6 +56,11 @@ const App = () => {
   };
 
   const toggleModal = e => {
+    if (e.code === 'Escape') {
+      setShowModal(!showModal);
+      return;
+    }
+
     const imgURL = e.currentTarget.getAttribute('largeimgurl');
     const altImg = e.currentTarget.getAttribute('alt');
     setShowModal(!showModal);
--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,18 +1,25 @@
 import s from './Modal.module.css';
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
+import { useEffect } from 'react';
 
-const Modal = ({ largeImageURL, alt, showModal }) => (
-  <div className={s.overlay}>
-    <div className={s.modal}>
-      <img
-        className={s.modalImg}
-        src={largeImageURL}
-        alt={alt}
-        onClick={showModal}
-      />
-    </div>
-  </div>
-);
+const Modal = ({ largeImageURL, alt, showModal }) => {
+  useEffect(() => {
+    window.addEventListener('keydown', showModal);
+    return () => {
+      window.removeEventListener('keydown', showModal);
+    };
+  });
+
+  return ReactDOM.createPortal(
+    <div className={s.overlay} onClick={showModal}>
+      <div className={s.modal}>
+        <img className={s.modalImg} src={largeImageURL} alt={alt} />
+      </div>
+    </div>,
+    document.getElementById('modal-root')
+  );
+};
 
 Modal.propTypes = {
   alt: PropTypes.string.isRequired,
